6ad2e39c53fa034172c0fad8e1a5cf10e70540dc
Santiago Blanco <santiagogbp@gmail.com>
2025-01-07 18:08:56 -0300

    no creo q sirva porq es muy complejo saber tiempos verbales y personas de pronombres etc

--- juego.py
+++ juego.py
@@ -1,1 +1,24 @@
+palabras = [
+    "el",
+    "la",
+    "lo",
+    "los",
+    "las",
+    "a",
+    "con",
+    "de",
+    "desde",
+    "en",
+    "entre",
+    "hasta",
+    "para",
+    "por",
+    "sobre",
+    "casa",
+    "auto",
+    "perro",
+    "jugar",
+    "matar",
+    ""
 
+]
